f2e60a3b3b83fe0671ea2cb38e1d4a7867cd1e42
PANDE <stevoxsane@gmail.com>
2026-02-19 10:59:49 +0300

    Added ability to generate file server process files from  templates to  paj

---
+++ lib/Regex/RegexHelpers.pm
@@ -0,0 +1,34 @@
+package Regex::RegexHelpers;
+
+use strict;
+use diagnostics;
+
+use lib "lib";
+
+# use Svelte::Funcs;
+use Regex::Regex;
+use base "Exporter";
+
+our @EXPORT_OK = qw(regex_template_writer);
+
+# TODO: Find out why opening a file from a package refuses and also passing in a fh refuses
+sub regex_template_writer {
+    my ( $fh, @match_expressions ) = @_;
+
+      # print("$template_filename \n @match_expressions \n");
+      # open my $fh, "<", $template_filename or die "Cannot open file $!";
+      my $template = "";
+    while ( my $line = <$fh> ) {
+        $template .= $line;
+    }
+
+    foreach my $regex_class (@match_expressions) {
+        my $regex = $regex_class->regex();
+        my $value = $regex_class->value();
+        $template =~ s/$regex/$value/g;
+    }
+
+    return $template;
+
+}
+1;
