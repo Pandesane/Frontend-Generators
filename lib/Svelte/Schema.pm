d29e4a79656f8d299464cf616fe64447c7329ce5
PANDE <stevoxsane@gmail.com>
2026-06-23 18:45:18 +0300

    Added Default templates for svelte generator

--- lib/Svelte/Schema.pm
+++ lib/Svelte/Schema.pm
@@ -10,11 +10,38 @@ use Svelte::Funcs;
 
 our @EXPORT_OK = qw(gen_resource_schema);
 
+sub gen_schema_fields {
+    my (@fields) = @_;
+    my $gen_fields = "";
+    foreach my $i (@fields) {
+        my ( $field, $field_type ) = split( ":", $i );
+        if ( $field_type ne "file" ) {
+            if ( $field_type eq "textarea" ) {
+                $gen_fields .= qq{
+            $field: v.pipe(v.string(), v.nonEmpty("$field cannot be blank."), v.minLength(15, "$field must be at least 15 characters long")),
+            };
+            } else {
+
+               $gen_fields .= qq{
+                 $field: v.pipe(v.string(), v.nonEmpty("$field can't be blank."), v.minLength(5, "$field must be at least 5 characters long")),
+            };
+            }
+        }
+
+    }
+    $gen_fields .= qq{
+       //id: v.union([v.string(), v.number()]),
+       // relation_id: v.union([v.string(), v.number()]),
+
+   };
+    return $gen_fields;
+}
+
 sub gen_resource_schema {
     my ( $file_name, $resource_name_singular_import,
         $resource_name_import, $resource_name, @fields )
       = @_;
-    my $gen_fields = Svelte::Funcs::gen_schema_fields(@fields);
+    my $gen_fields = gen_schema_fields(@fields);
     my $template   = qq{
      import * as z from "zod";
 
